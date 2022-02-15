4635818a7d3be1d9d2170e5db176985330131862
nghia123z1 <97504338+nghia123z1@users.noreply.github.com>
2022-02-15 14:59:26 +0700

    baithuchanh1502
    baithuchanh1502

---
+++ app35.java
@@ -0,0 +1,16 @@
+nhập  java.util.Scanner ;
+
+ lớp  công khai App35 {
+    public  static  void  main ( String [] args ) ném  Exception {
+        Máy quét sc = Máy quét mới  ( Hệ thống . In);
+        int a, b;
+        Hệ thống . ra ngoài . println ( " Nhap a: " );
+        a = sc . nextInt ();
+        Hệ thống . ra ngoài . println ( " Nhap b: " );
+        b = sc . nextInt ();
+        nếu (a > b)
+        Hệ thống . ra ngoài . println (a);
+         hệ thống khác . ra ngoài . in (b);
+
+    }
+}
