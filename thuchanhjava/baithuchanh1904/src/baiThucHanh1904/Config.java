25b00f88283bdcdc4f56077d0e33fb8c3a2522ab
nghia123z1 <97504338+nghia123z1@users.noreply.github.com>
2022-04-19 21:00:36 +0700

    Add files via upload

---
+++ thuchanhjava/baithuchanh1904/src/baiThucHanh1904/Config.java
@@ -0,0 +1,11 @@
+public package baiThucHanh1904;
+
+public class Configs {
+    public static final int LINH = 1;
+  
+    public static final long LUONG_NHAN_VIEN_FULL_TIME = 10000000; 
+  
+    public static final long LUONG_NHAN_VIEN_PART_TIME_MOI_GIO = 100000; 
+}class Config {
+    
+}
